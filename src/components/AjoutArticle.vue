8a4fe233fe9e5a3520b5fc5a13cee908ede7b122
Abdoulrachard <Abdoulrachard@gmail.com>
2024-07-24 17:01:24 +0100

    updated profil section and cand added the different step to add an articles

--- src/components/AjoutArticle.vue
+++ src/components/AjoutArticle.vue
@@ -3,64 +3,95 @@
 </script>
 <template>
 
-    <article class="docs-article " id="section-3">
+    <article class="docs-article" id="section-3">
         <header class="docs-header">
-            <h1 class="docs-heading">Ajout d'articles</h1>
+            <h1 class="docs-heading">Publication d'articles</h1>
             <section class="docs-intro">
                 <p>
-                    Vous avez oublié votre mot de passe ? Pas de souci ! Suivez ces étapes simples pour réinitialiser
-                    votre mot de passe et accéder à nouveau à votre compte ShapLocation en toute sécurité.
+                    Découvrez comment ajouter des articles à vendre sur ShapLocation. Suivez ces étapes simples pour
+                    publier vos articles et les rendre visibles aux acheteurs potentiels.
                 </p>
             </section>
         </header>
         <section class="docs-section">
             <h2 class="vt-doc pad-top">Comment ajouter un article ?</h2>
-            <p>Pour ajouter un article sur shaplocation suivez les étapes suivantes :</p>
+            <p>Pour ajouter un article sur ShapLocation, suivez les étapes suivantes :</p>
             <div class="row mb-3 mt-5">
                 <div class="col-md-12">
                     <ol>
                         <li>
-                            Assurez-vous que vous êtes inscrit en tant que marchand ou
-                            propriétaire. Si ce n'est pas le cas, allez dans votre
-                            profil et changez le type de compte en suivant les
-                            instructions.
+                            Assurez-vous que vous êtes inscrit en tant que marchand ou propriétaire. Si ce n'est pas le
+                            cas, allez dans votre profil et changez le type de compte en suivant les instructions.
                         </li>
                         <li>
-                            Accédez à la plateforme en allant sur shaplocation.com.
+                            Accédez à la plateforme en allant sur <a class="link-primary"
+                                href="https://shaplocation.com" target="_blank">shaplocation.com</a>.
                         </li>
-                        <li>Connectez-vous à votre compte.</li>
+                        <li>Connectez-vous à votre compte si ce n'est pas encore le cas.</li>
                         <li>
-                            Accédez au tableau de bord en cliquant sur l'icône de
-                            tableau de bord en forme de carré en haut. Une fois cliquée,
-                            vous accédez à votre tableau de bord où vos propres articles
-                            seront affichés. Par défaut, il n'y a aucun article.
-                        </li>
-                        <li>
-                            Ajoutez un article en cliquant sur le bouton "Ajouter un
-                            article" en haut à droite. Vous verrez les instructions pour
-                            publier un article.
+                            Accédez au tableau de bord en cliquant sur l'icône de tableau de bord en forme de carré en
+                            haut. Vous serez redirigé vers votre tableau de bord où vos articles seront affichés. Par
+                            défaut, il n'y a aucun article.
                         </li>
                         <li>
-                            Publiez un article en choisissant le type d'article que vous
-                            souhaitez publier et en suivant les instructions restantes.
+                            Ajoutez un article en cliquant sur le bouton "Ajouter un article" en haut à droite. Suivez
+                            les instructions pour publier un article.
+                            <ol type="a">
+                                <li><span class="fw-medium mx-1">Informations Générales :</span> Ajoutez des
+                                    informations de base telles que le titre, la description, le prix, etc.</li>
+                                <li><span class="fw-medium mx-1">Localisation :</span> Indiquez l'emplacement exact, y
+                                    compris l'adresse, le quartier, la ville, et toute information géographique
+                                    pertinente.</li>
+                                <li><span class="fw-medium mx-1">Caractéristiques :</span> Renseignez les spécifications
+                                    et les attributs uniques de votre article ainsi que tout autre aspect pertinent qui
+                                    le distingue.</li>
+                                <li><span class="fw-medium mx-1">Images :</span> Téléchargez des images de haute qualité
+                                    pour illustrer l'article. Assurez-vous d'inclure des vues détaillées et variées pour
+                                    une meilleure compréhension visuelle.</li>
+                                <li><span class="fw-medium mx-1">Résumé :</span> Un résumé de toutes les
+                                    informations fournies, incluant les détails clés comme le titre, la description, le
+                                    prix, la localisation, et les images. Un aperçu de l'article avec toutes ces
+                                    informations.</li>
+                            </ol>
+                        </li>
+                        <li>
+                            Publiez l'article en choisissant le type d'article que vous souhaitez publier et en suivant
+                            les instructions restantes.
+                            <p><span class="fw-medium">NB:</span> Si vous ne voyez pas les types d'articles à ce niveau,
+                                alors vous devez compléter les informations de votre profil d'abord. Après avoir mis à
+                                jour vos informations, les types d'articles apparaîtront.</p>
+                            <RouterLink to="/profil" class="link-primary">Decouvrez comment completer les informations de profile ici</RouterLink>
+                        </li>
+                    </ol>
+                    <h4 class="vt-doc pad-top">Paiement des Frais de Publication</h4>
+                    <ol start="7">
+                        <li>
+                            Après avoir choisi le type d'article, validez et entrez les informations générales, de
+                            localisation, de caractéristiques, et les images.
+                        </li>
+                        <li>
+                            Avant de publier, vous devrez payer les frais de publication. Les frais sont calculés en
+                            pourcentage du prix de vente de l'article.
+                            <ol type="a">
+                                <li>Cliquez sur le bouton affichant le montant à payer.</li>
+                                <li>Vous serez redirigé vers la page de paiement.</li>
+                                <li>Sélectionnez le mode de paiement et validez le paiement.</li>
+                            </ol>
+                        </li>
+                        <li>
+                            Une fois les frais de publication payés, votre article sera automatiquement publié sur la
+                            plateforme et sera disponible sur le marché.
                         </li>
                     </ol>
                     <p>
-                        <span class="fw-medium">NB:</span> Contactez-nous si vous
-                        rencontrez des difficultés par email, par téléphone ou en
-                        écrivant dans ce forum.
+                        <span class="fw-medium">NB:</span> Si vous rencontrez des difficultés, contactez-nous par email,
+                        par téléphone ou en écrivant dans ce forum.
                     </p>
                 </div>
-                <!-- <div class="col-md-6 d-flex justify-content-center align-items-center">
-                    <div class=" simplelightbox-gallery  ">
-                        <img class="figure-img img-fluid shadow rounded" src="@/assets/images/profil.png"
-                            alt="Image de la page de profil" title="Page de profil" />
-                    </div>
-                </div> -->
             </div>
-
         </section>
     </article>
+
 
 </template>
 
--- src/components/Profil.vue
+++ src/components/Profil.vue
@@ -3,9 +3,9 @@
 </script>
 <template>
 
-    <article class="docs-article " id="section-2">
+    <article class="docs-article" id="section-2">
         <header class="docs-header">
-            <h1 class="docs-heading">Profil</h1>
+            <h1 class="docs-heading">Configuration de profil</h1>
             <section class="docs-intro">
                 <p>
                     Découvrez comment mettre à jour et gérer votre profil sur ShapLocation. Assurez-vous que vos
@@ -16,40 +16,84 @@
         </header>
         <section class="docs-section" id="profil-config">
             <h2 class="vt-doc pad-top">Comment mettre à jour le profil ?</h2>
-            <p>Pour mettre à jour son profile sur shaplocation suivez les étapes suivantes :</p>
+            <p>Pour mettre à jour son profil sur ShapLocation, suivez les étapes suivantes :</p>
             <div class="row mb-3 mt-5">
                 <div class="col-md-6">
-
                     <ol>
                         <li>Accédez à la Plateforme - Allez sur <a class="link-primary"
                                 href="https://shaplocation.com/dashboard/user/profile"
                                 target="_blank">shaplocation.com</a></li>
                         <li>Connectez-vous à votre compte.</li>
-                        <li>Accédez à votre profil en cliquant sur profil haut à droite.
-                        </li>
+                        <li>Accédez à votre profil en cliquant sur profil en haut à droite.</li>
                         <li>
                             Mettez à jour vos informations :
                             <ul>
                                 <li><span class="fw-medium">Nom et Prénom :</span> Modifiez votre prénom et nom de
-                                    famille si
-                                    nécessaire.</li>
+                                    famille si nécessaire.</li>
                                 <li><span class="fw-medium">Photo de Profil :</span> Téléchargez une nouvelle photo de
-                                    profil pour une
-                                    meilleure identification visuelle.</li>
+                                    profil pour une meilleure identification visuelle.</li>
                                 <li><span class="fw-medium">Adresse Email :</span> Changez votre adresse email pour
-                                    recevoir des
-                                    notifications à la bonne adresse.</li>
+                                    recevoir des notifications à la bonne adresse.</li>
                                 <li><span class="fw-medium">Numéro de Téléphone :</span> Mettez à jour votre numéro de
-                                    téléphone pour
-                                    les vérifications et les notifications par SMS.</li>
+                                    téléphone pour les vérifications et les notifications par SMS.</li>
                                 <li><span class="fw-medium">Type de compte :</span> Modifiez le type de compte si
                                     nécessaire.</li>
                             </ul>
                         </li>
-                        <li>Enregistrez les modifications en cliquant sur le bouton "Modifier le profil".
-                        </li>
+                        <li>Enregistrez les modifications en cliquant sur le bouton "Modifier le profil".</li>
                         <li>Vérifiez les mises à jour - Assurez-vous que les modifications ont été correctement
                             enregistrées en consultant votre profil.</li>
+                        <li>
+                            <span class="fw-medium">Devenir commerçant :</span> Suivez ces étapes supplémentaires :
+                            <ol>
+                                <li>Cliquez sur le bouton "Devenir commerçant". Vous serez redirigé vers la page de
+                                    changement de compte.</li>
+                                <li>Choisissez si vous êtes une entreprise ou un démarcheur :
+                                    <ul>
+                                        <li>
+                                            <span class="fw-medium">Démarcheur :</span>
+                                            <ol>
+                                                <li>Sélectionnez "Démarcheur" et un formulaire apparaîtra en bas.</li>
+                                                <li>Entrez votre Identifiant Fiscal Unique (IFU) et choisissez le type
+                                                    de pièce (Certificat d'identification personnelle (CIP) ou Carte
+                                                    nationale d'identité (CNI)).</li>
+                                                <li>Saisissez le numéro de la pièce et cliquez sur le bouton "Suivant".
+                                                </li>
+                                                <li>Vous serez redirigé vers une page où vous devez ajouter les
+                                                    documents et pièces justificatives (le document IFU et celui de la
+                                                    carte).</li>
+                                                <li>Cliquez sur "Suivant". Les informations seront automatiquement
+                                                    enregistrées et vous serez redirigé vers la page d'accueil.</li>
+                                            </ol>
+                                        </li>
+                                        <li>
+                                            <span class="fw-medium">Entreprise :</span>
+                                            <ol>
+                                                <li>Sélectionnez "Entreprise" et un formulaire apparaîtra en bas.</li>
+                                                <li>Entrez les informations suivantes : Identifiant Fiscal Unique (IFU),
+                                                    Nom, Extrait du Registre de Commerce et de Crédit Mobilier (RCCM),
+                                                    et Description.</li>
+                                                <li>Cliquez sur le bouton "Suivant".</li>
+                                                <li>Vous serez redirigé vers une page avec un formulaire demandant des
+                                                    informations supplémentaires : Pays, Départements, Communes,
+                                                    Arrondissements, Quartiers, et Adresse.</li>
+                                                <li>Remplissez le formulaire et cliquez sur "Suivant".</li>
+                                                <li>Vous serez redirigé vers une page où vous devez sélectionner les
+                                                    types d'articles que vous souhaitez publier sur ShapLocation.</li>
+                                                <li>Sélectionnez les types d'articles et cliquez sur "Suivant".</li>
+                                                <li>Vous serez redirigé vers une page où vous devez ajouter les
+                                                    documents et pièces justificatives (Identifiant Fiscal Unique (IFU)
+                                                    et Extrait du Registre de Commerce et de Crédit Mobilier (RCCM)).
+                                                </li>
+                                                <li>Ajoutez les documents et cliquez sur "Suivant". Les informations
+                                                    seront automatiquement enregistrées et vous serez redirigé vers la
+                                                    page d'accueil.</li>
+                                            </ol>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ol>
+                        </li>
                     </ol>
                     <p>
                         <span class="fw-medium">NB:</span> Si vous rencontrez des difficultés, contactez-nous par email,
@@ -63,9 +107,9 @@
                     </div>
                 </div>
             </div>
-
         </section>
     </article>
+
 
 </template>
 
