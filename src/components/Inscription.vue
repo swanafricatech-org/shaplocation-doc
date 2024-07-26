299f7e361a1767c5812db5cc98d3e2359200f4c0
Abdoulrachard <Abdoulrachard@gmail.com>
2024-07-26 17:08:12 +0100

    added other steps for register when you are not simple user

--- src/components/Inscription.vue
+++ src/components/Inscription.vue
@@ -9,130 +9,153 @@
             <section class="docs-intro">
                 <p>
                     Découvrez comment créer un compte sur ShapLocation pour profiter de toutes ses fonctionnalités.
-                    Suivez ces
-                    étapes simples pour vous inscrire et commencer à utiliser notre plateforme.
+                    Suivez ces étapes simples pour vous inscrire et commencer à utiliser notre plateforme.
                 </p>
             </section>
         </header>
-        <section class="docs-section ">
+        <section class="docs-section">
             <h2 class="vt-doc pad-top">Comment s'inscrire ?</h2>
-            <p>Pour s'inscrire sur ShapLocation, vous avez deux (02) modes d'inscription :
+            <p>Pour s'inscrire sur ShapLocation, vous avez deux (02) modes d'inscription :</p>
             <ul>
-                <li><a href="#ins-mode-1" class="link-primary"> Incription complète</a></li>
+                <li><a href="#ins-mode-1" class="link-primary">Inscription par formulaire</a></li>
                 <li><a href="#ins-mode-2" class="link-primary">Inscription par Google</a></li>
             </ul>
-            Voici comment procéder pour chaque méthode :
-            </p>
+            <p>Voici comment procéder pour chaque méthode :</p>
+
+            <!-- Inscription complète -->
             <div class="row mb-3 mt-5" id="ins-mode-1">
                 <div class="col-md-6">
-                    <h5><span>#</span> Inscription complète</h5>
+                    <h3><span>#</span> Inscription par formulaire</h3>
                     <ol>
                         <li>Accédez à la Plateforme <a href="http://shaplocation.com" target="_blank"
-                                rel="noopener noreferrer" class="link-primary">shaplocation.com</a>
-                        </li>
-                        <li> Allez sur la page
-                            de connexion en cliquant <a class="link-primary" href="https://shaplocation.com/auth/signin"
-                                target="_blank">ici</a></li>
-                        <li>
-                            Vous verrez le lien "Créer un compte" en bas de la page,
-                            cliquer la dessus. vous pouvez aller directement sur la page d'inscription en cliquant <a
-                                class="link-primary" href="https://shaplocation.com/auth/signup" target="_blank">ici</a>
-                        </li>
+                                rel="noopener noreferrer" class="link-primary">shaplocation.com</a></li>
+                        <li>Allez sur la page de connexion en cliquant <a class="link-primary"
+                                href="https://shaplocation.com/auth/signin" target="_blank">ici</a></li>
+                        <li>Vous verrez le lien "Créer un compte" en bas de la page, cliquez dessus. Vous pouvez aller
+                            directement sur la page d'inscription en cliquant <a class="link-primary"
+                                href="https://shaplocation.com/auth/signup" target="_blank">ici</a></li>
                         <li>
                             Fournissez les Informations Personnelles :
                             <ul>
+                                <li>Nom et prénom : Entrez votre prénom et nom.</li>
+                                <li>Numéro de téléphone : Indiquez un numéro de téléphone actif pour la vérification et
+                                    les notifications.</li>
                                 <li>
-                                    Nom et prénom : Entrez votre prénom et nom .
+                                    Types de compte : Sélectionnez le type de compte que vous voulez créer (Utilisateur,
+                                    Marchand, Propriétaire).<br />
+                                    <span class="fw-medium">NB: Le démarcheur est un marchand.</span>
                                 </li>
                                 <li>
-                                    Numéro de téléphone : Indiquez un numéro de téléphone
-                                    actif pour la vérification et les notifications.
+                                    Mot de passe : Choisissez un mot de passe sécurisé et mémorable. <br />
+                                    <span class="fw-medium">Note : Le mot de passe doit contenir des lettres majuscules,
+                                        minuscules, des chiffres et des symboles.</span>
                                 </li>
-                                <li>
-                                    Types de compte : Sélectionnez le type de compte que
-                                    vous voulez créer (Utilisateur, Marchand,
-                                    Propriétaire).<br /><span class="fw-medium">
-                                        NB: Le démarcheur est un marchand.</span>
-                                </li>
-                                <li>
-                                    Mot de passe : Choisissez un mot de passe sécurisé et
-                                    mémorable. <br />
-                                    <span class="fw-medium">Note : Le mot de passe doit contenir des lettres
-                                        majuscules, minuscules, des chiffres et des
-                                        symboles.</span>
-                                </li>
-                                <li>
-                                    Confirmation du mot de passe : Saisissez à nouveau le
-                                    mot de passe pour confirmation.
-                                </li>
+                                <li>Confirmation du mot de passe : Saisissez à nouveau le mot de passe pour
+                                    confirmation.</li>
                             </ul>
                         </li>
-                        <li>
-                            Acceptez les Conditions d'Utilisation - Lisez les conditions
-                            d'utilisation et la politique de confidentialité de
-                            ShapLocation.
-                        </li>
-                        <li>
-                            Vérifiez l'Inscription - Cliquez sur le bouton "S'inscrire".
-                            Vous recevrez un SMS avec un code de vérification. Entrez le
-                            code au niveau de la page de vérification.
-                        </li>
-
+                        <li>Acceptez les Conditions d'Utilisation - Lisez les conditions d'utilisation et la politique
+                            de confidentialité de ShapLocation.</li>
+                        <li>Vérifiez l'Inscription - Cliquez sur le bouton "S'inscrire". Vous recevrez un SMS avec un
+                            code de vérification. Entrez le code au niveau de la page de vérification.</li>
                     </ol>
+
+                    <!-- Étapes supplémentaires pour Marchand et Propriétaire -->
+                    <div class="callout-block callout-block-info mt-4">
+
+                        <div class="content">
+                            <h3 class="callout-title ">
+                                <span class="callout-icon-holder me-1">
+                                    <i class="fas fa-info-circle"></i>
+                                </span>
+                                Étapes supplémentaires pour Marchand et Propriétaire :
+                            </h3>
+
+                        </div>
+                    </div>
+                    <p>Lorsque vous choisissez "Marchand" ou "Propriétaire" comme type de compte, des étapes
+                        supplémentaires sont nécessaires :</p>
+                    <div>
+                        <h4 class="fw-medium">Marchand :</h4>
+                        <ol>
+                            <li>Sélectionnez "Marchand" lors de la création de compte.</li>
+                            <li>Un formulaire supplémentaire apparaîtra.</li>
+                            <li>Entrez votre Identifiant Fiscal Unique (IFU) et choisissez le type de pièce (CIP ou
+                                CNI).</li>
+                            <li>Saisissez le numéro de la pièce et cliquez sur "Suivant".</li>
+                            <li>Ajoutez les documents justificatifs (document IFU et celui de la carte).<br>
+                                <span class="fw-medium">NB :</span> Les documents doivent correspondre aux informations
+                                fournies et à la pièce d'identité.
+                            </li>
+                            <li>Cliquez sur "Suivant". Les informations seront automatiquement enregistrées et vous
+                                serez redirigé vers la page d'accueil.</li>
+                        </ol>
+                        <h4 class="fw-medium">Propriétaire :</h4>
+                        <ol>
+                            <li>Sélectionnez "Propriétaire" lors de la création de compte.</li>
+                            <li>Un formulaire supplémentaire apparaîtra.</li>
+                            <li>Entrez les informations suivantes : Identifiant Fiscal Unique (IFU), Nom, Extrait du
+                                Registre de Commerce et de Crédit Mobilier (RCCM), et Description.</li>
+                            <li>Cliquez sur "Suivant".</li>
+                            <li>Un autre formulaire demandera des informations supplémentaires : Pays, Départements,
+                                Communes, Arrondissements, Quartiers, et Adresse.</li>
+                            <li>Remplissez le formulaire et cliquez sur "Suivant".</li>
+                            <li>Sélectionnez les types d'articles que vous souhaitez publier sur ShapLocation et cliquez
+                                sur "Suivant".</li>
+                            <li>Ajoutez les documents justificatifs (IFU et RCCM).<br>
+                                <span class="fw-medium">NB :</span> Les documents doivent correspondre aux informations
+                                fournies et à la pièce d'identité.
+                            </li>
+                            <li>Cliquez sur "Suivant". Les informations seront automatiquement enregistrées et vous
+                                serez redirigé vers la page d'accueil.</li>
+                        </ol>
+                    </div>
                 </div>
                 <div class="col-md-6 d-flex justify-content-center align-items-center flex-column gap-4">
-                    <div class=" gallery  ">
+                    <div class="gallery">
                         <img class="figure-img img-fluid shadow rounded" src="@/assets/images/register-link.png"
                             alt="Image de la page d'inscription par google" title="Page de d'inscription" />
                     </div>
-                    <div class=" gallery  ">
+                    <div class="gallery">
                         <img class="figure-img img-fluid shadow rounded" src="@/assets/images/register-1.png"
                             alt="Image de la page d'inscription par google" title="Page de d'inscription" />
                     </div>
                 </div>
             </div>
+
+            <!-- Inscription par Google -->
             <div class="row mb-3 mt-5" id="ins-mode-2">
                 <div class="col-md-6">
-                    <h5><span>#</span> Inscription par Google</h5>
+                    <h3><span>#</span> Inscription par Google</h3>
                     <ol>
                         <li>Accédez à la Plateforme <a href="http://shaplocation.com" target="_blank"
-                                rel="noopener noreferrer" class="link-primary">shaplocation.com</a>
-                        </li>
-                        <li> Allez sur la page
-                            de connexion en cliquant <a class="link-primary" href="https://shaplocation.com/auth/signin"
-                                target="_blank">ici</a>
-                        </li>
-                        <li>
-                            Vous verrez le lien "Créer un compte" en bas de la page,
-                            cliquer la dessus. vous pouvez aller directement sur la page d'inscription en cliquant <a
-                                class="link-primary" href="https://shaplocation.com/auth/signup" target="_blank">ici</a>
-                        </li>
-                        <li> vous verrez
-                            le button "Se connecter avec
-                            Google" juste en bas .</li>
-                        <li>Cliquez sur le button "Se connecter avec Google".</li>
+                                rel="noopener noreferrer" class="link-primary">shaplocation.com</a></li>
+                        <li>Allez sur la page de connexion en cliquant <a class="link-primary"
+                                href="https://shaplocation.com/auth/signin" target="_blank">ici</a></li>
+                        <li>Vous verrez le lien "Créer un compte" en bas de la page, cliquez dessus. Vous pouvez aller
+                            directement sur la page d'inscription en cliquant <a class="link-primary"
+                                href="https://shaplocation.com/auth/signup" target="_blank">ici</a></li>
+                        <li>Vous verrez le bouton "Se connecter avec Google" juste en bas.</li>
+                        <li>Cliquez sur le bouton "Se connecter avec Google".</li>
                         <li>Une fenêtre de connexion Google s'ouvrira. Entrez vos informations de connexion Google
                             (adresse email et mot de passe) et suivez les instructions pour vous authentifier.</li>
                         <li>Accordez les autorisations - Si c'est la première fois que vous utilisez votre compte Google
                             pour vous connecter à ShapLocation, vous serez invité à accorder certaines autorisations.
                             Cliquez sur "Accepter" ou "Autoriser".</li>
-                        <li>Finalisez la Connexion - Une fois les autorisations accordées,vous serez inscrire
-                            automatiquement
-                            avec votre compte Google. Ensuite vous serez redirigé vers
-                            la page de mise à jour de profil . <br> Pour en savoir plus sur la mise à jour de consultez
-                            notre guide de <RouterLink to="/profil" class="link-primary mx-2">mise à jour de profil
-                            </RouterLink>pour plus d'instructions.
+                        <li>Finalisez la Connexion - Une fois les autorisations accordées, vous serez inscrit
+                            automatiquement avec votre compte Google. Ensuite, vous serez redirigé vers la page de mise
+                            à jour de profil.<br>
+                            Pour en savoir plus sur la mise à jour, consultez notre guide de <RouterLink to="/profil"
+                                class="link-primary mx-2">mise à jour de profil</RouterLink> pour plus d'instructions.
                         </li>
-
-
                     </ol>
                 </div>
                 <div class="col-md-6 d-flex justify-content-center align-items-center flex-column gap-4">
-                    <div class=" gallery  ">
+                    <div class="gallery">
                         <img class="figure-img img-fluid shadow rounded" src="@/assets/images/register-link.png"
                             alt="Image de la page d'inscription par google" title="Page de d'inscription" />
                     </div>
-                    <div class=" gallery  ">
+                    <div class="gallery">
                         <img class="figure-img img-fluid shadow rounded" src="@/assets/images/register-2.png"
                             alt="Image de la page d'inscription par google" title="Page de d'inscription" />
                     </div>
@@ -140,6 +163,7 @@
             </div>
         </section>
     </article>
+
 
 </template>
 
