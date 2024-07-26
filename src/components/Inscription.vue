d2d4ce4a7d4f64a0680cd31373b3424277bd132a
Abdoulrachard <Abdoulrachard@gmail.com>
2024-07-26 17:55:20 +0100

    updated register steps

--- src/components/Inscription.vue
+++ src/components/Inscription.vue
@@ -62,67 +62,76 @@
 
                     <!-- Étapes supplémentaires pour Marchand et Propriétaire -->
                     <div class="callout-block callout-block-info mt-4">
-
                         <div class="content">
-                            <h3 class="callout-title ">
+                            <h3 class="callout-title">
                                 <span class="callout-icon-holder me-1">
                                     <i class="fas fa-info-circle"></i>
                                 </span>
                                 Étapes supplémentaires pour Marchand et Propriétaire :
                             </h3>
-
                         </div>
                     </div>
                     <p>Lorsque vous choisissez "Marchand" ou "Propriétaire" comme type de compte, des étapes
                         supplémentaires sont nécessaires :</p>
-                    <div>
-                        <h4 class="fw-medium">Marchand :</h4>
-                        <ol>
-                            <li>Sélectionnez "Marchand" lors de la création de compte.</li>
-                            <li>Un formulaire supplémentaire apparaîtra.</li>
-                            <li>Entrez votre Identifiant Fiscal Unique (IFU) et choisissez le type de pièce (CIP ou
-                                CNI).</li>
-                            <li>Saisissez le numéro de la pièce et cliquez sur "Suivant".</li>
-                            <li>Ajoutez les documents justificatifs (document IFU et celui de la carte).<br>
-                                <span class="fw-medium">NB :</span> Les documents doivent correspondre aux informations
-                                fournies et à la pièce d'identité.
-                            </li>
-                            <li>Cliquez sur "Suivant". Les informations seront automatiquement enregistrées et vous
-                                serez redirigé vers la page d'accueil.</li>
-                        </ol>
-                        <h4 class="fw-medium">Propriétaire :</h4>
-                        <ol>
-                            <li>Sélectionnez "Propriétaire" lors de la création de compte.</li>
-                            <li>Un formulaire supplémentaire apparaîtra.</li>
-                            <li>Entrez les informations suivantes : Identifiant Fiscal Unique (IFU), Nom, Extrait du
-                                Registre de Commerce et de Crédit Mobilier (RCCM), et Description.</li>
-                            <li>Cliquez sur "Suivant".</li>
-                            <li>Un autre formulaire demandera des informations supplémentaires : Pays, Départements,
-                                Communes, Arrondissements, Quartiers, et Adresse.</li>
-                            <li>Remplissez le formulaire et cliquez sur "Suivant".</li>
-                            <li>Sélectionnez les types d'articles que vous souhaitez publier sur ShapLocation et cliquez
-                                sur "Suivant".</li>
-                            <li>Ajoutez les documents justificatifs (IFU et RCCM).<br>
-                                <span class="fw-medium">NB :</span> Les documents doivent correspondre aux informations
-                                fournies et à la pièce d'identité.
-                            </li>
-                            <li>Cliquez sur "Suivant". Les informations seront automatiquement enregistrées et vous
-                                serez redirigé vers la page d'accueil.</li>
-                        </ol>
-                    </div>
+                    <ol start="7">
+                        <li>Après la vérification de votre inscription, vous serez redirigé vers une page où vous devez
+                            fournir des informations supplémentaires.</li>
+                        <li>Choisissez si vous êtes une entreprise ou un démarcheur :
+                            <ul>
+                                <li>
+                                    <span class="fw-medium">Démarcheur :</span>
+                                    <ol>
+                                        <li>Sélectionnez "Démarcheur" et un formulaire apparaîtra en bas.</li>
+                                        <li>Entrez votre Identifiant Fiscal Unique (IFU) et choisissez le type de pièce
+                                            (Certificat d'identification personnelle (CIP) ou Carte nationale d'identité
+                                            (CNI)).</li>
+                                        <li>Saisissez le numéro de la pièce et cliquez sur le bouton "Suivant".</li>
+                                        <li>Vous serez redirigé vers une page où vous devez ajouter les documents et
+                                            pièces justificatives (le document IFU et celui de la carte).</li>
+                                        <li>Cliquez sur "Suivant". Les informations seront automatiquement enregistrées
+                                            et vous serez redirigé vers la page d'accueil.</li>
+                                    </ol>
+                                </li>
+                                <li>
+                                    <span class="fw-medium">Entreprise :</span>
+                                    <ol>
+                                        <li>Sélectionnez "Entreprise" et un formulaire apparaîtra en bas.</li>
+                                        <li>Entrez les informations suivantes : Identifiant Fiscal Unique (IFU), Nom,
+                                            Extrait du Registre de Commerce et de Crédit Mobilier (RCCM), et
+                                            Description.</li>
+                                        <li>Cliquez sur le bouton "Suivant".</li>
+                                        <li>Vous serez redirigé vers une page avec un formulaire demandant des
+                                            informations supplémentaires : Pays, Départements, Communes,
+                                            Arrondissements, Quartiers, et Adresse.</li>
+                                        <li>Remplissez le formulaire et cliquez sur "Suivant".</li>
+                                        <li>Vous serez redirigé vers une page où vous devez sélectionner les types
+                                            d'articles que vous souhaitez publier sur ShapLocation.</li>
+                                        <li>Sélectionnez les types d'articles et cliquez sur "Suivant".</li>
+                                        <li>Vous serez redirigé vers une page où vous devez ajouter les documents et
+                                            pièces justificatives (Identifiant Fiscal Unique (IFU) et Extrait du
+                                            Registre de Commerce et de Crédit Mobilier (RCCM)).</li>
+                                        <li>Ajoutez les documents et cliquez sur "Suivant". Les informations seront
+                                            automatiquement enregistrées et vous serez redirigé vers la page d'accueil.
+                                        </li>
+                                    </ol>
+                                </li>
+                            </ul>
+                        </li>
+                    </ol>
                 </div>
                 <div class="col-md-6 d-flex justify-content-center align-items-center flex-column gap-4">
                     <div class="gallery">
                         <img class="figure-img img-fluid shadow rounded" src="@/assets/images/register-link.png"
-                            alt="Image de la page d'inscription par google" title="Page de d'inscription" />
+                            alt="Image de la page d'inscription par google" title="Page d'inscription" />
                     </div>
                     <div class="gallery">
                         <img class="figure-img img-fluid shadow rounded" src="@/assets/images/register-1.png"
-                            alt="Image de la page d'inscription par google" title="Page de d'inscription" />
+                            alt="Image de la page d'inscription par google" title="Page d'inscription" />
                     </div>
                 </div>
             </div>
 
+            <hr>
             <!-- Inscription par Google -->
             <div class="row mb-3 mt-5" id="ins-mode-2">
                 <div class="col-md-6">
